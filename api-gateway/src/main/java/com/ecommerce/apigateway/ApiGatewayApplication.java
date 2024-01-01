9977af919facfae6da724e760447a2d405171561
Achraf-haddar <achraf.haddar@supcom.tn>
2024-01-01 14:12:48 +0200

    Add containerization and docker compose

--- api-gateway/src/main/java/com/ecommerce/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/ecommerce/apigateway/ApiGatewayApplication.java
@@ -1,5 +1,6 @@
 package com.ecommerce.apigateway;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
@@ -7,6 +8,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 public class ApiGatewayApplication {
     public static void main(String[] args){
-        SpringBootApplication.run(ApiGatewayApplication.class, args);
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
 }
